f24df06fa021fbf9d0069e16de3f285f421c34e2
Cédric Gémy <dev@activ.studio>
2019-11-11 15:11:43 +0100

    [BUG] no SMTP on tuxfamily > using lgm@afgral.org on ovh

--- mailer.php
+++ mailer.php
@@ -2,10 +2,15 @@
 use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
 
+$host = "ssl0.ovh.net";
+$port = 465;
+$user = "lgm@afgral.org";
+$pass = "pasMailSurTuxFamily2019";
+
 if (array_key_exists('first_name', $_POST)) {
     $mail = new PHPMailer;
     // only if using non local smtp
-    /*$mail->IsSMTP();
+    $mail->IsSMTP();
     $mail->SMTPDebug = 2;
     $mail->SMTPAuth = true;
     $mail->SMTPSecure = 'ssl';
@@ -13,12 +18,12 @@ if (array_key_exists('first_name', $_POST)) {
     $mail->Port = $port;
 
     $mail->Username = $user;
-    $mail->Password = $pass;*/
+    $mail->Password = $pass;
 
-    $mail->setFrom($mail_adr, $mail_name);
-    $mail->addReplyTo($mail_adr, $mail_name);
-    $mail->addAddress('contact@afgral.org', 'Afgral LGM'); // adds new recipients
-    $mail->Subject = 'PHPMailer SMTP message';
+    $mail->setFrom($_POST['email'], $_POST['first_name']);
+    $mail->addReplyTo($_POST['email'], $_POST['first_name']);
+    $mail->addAddress('lgm@afgral.org', 'Afgral LGM'); // adds new recipients
+    $mail->Subject = 'LGM Website';
     //$mail->msgHTML(file_get_contents('message.html'), __DIR__);
     //$mail->msgHTML("file_get_contents('message.html')", __DIR__);
     $mail->isHTML(false);
