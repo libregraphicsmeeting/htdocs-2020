94f997165a28cce9db953e52a4abb6211fd6ecf7
Gemy Cédric <cedric@cgemy.com>
2020-01-20 15:15:20 +0100

    [BUG] contact form sends error 500, missing ;

--- mailer.php
+++ mailer.php
@@ -1,8 +1,9 @@
 <?php
+
 require_once("base.php");
 
 if (array_key_exists('first_name', $_POST)) {
-    $mail = new PHPMailer()
+    $mail = new PHPMailer();
 
     $mail->setFrom($_POST['email'], $_POST['first_name']);
     $mail->addReplyTo($_POST['email'], $_POST['first_name']);
@@ -17,16 +18,6 @@ if (array_key_exists('first_name', $_POST)) {
         #echo 'Mailer Error: ' . $mail->ErrorInfo;
     } else {
         # Message sent!
-        header('Location:en/contact_sent.html');
-    }
-}
-
-if (array_key_exists('first_name', $_POST)) {
-    $mail = new lgmMailer()
-    if (!$mail->send($_POST['email'], $_POST['first_name'], "CONTACT", $_POST['message'])) {
-        header('Location:en/contact_fail.html');
-        #echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
         header('Location:en/contact_sent.html');
     }
 }
